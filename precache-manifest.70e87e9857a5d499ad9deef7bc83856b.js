f91fbce766e3ee4bab764a9626ef969d34f174fd
Dyana Monroy <dyana.monroy17@live.com>
2019-12-03 15:32:50 -0500

    Updates

---
+++ precache-manifest.70e87e9857a5d499ad9deef7bc83856b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52955f70444b634d8cc53c6a7e03647",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "4614da1f17469c57e9b7",
+    "url": "/Monsters-Rolodex/static/css/main.accb69f4.chunk.css"
+  },
+  {
+    "revision": "5ffb6fab3c6fb8afc674",
+    "url": "/Monsters-Rolodex/static/js/2.f6373902.chunk.js"
+  },
+  {
+    "revision": "4614da1f17469c57e9b7",
+    "url": "/Monsters-Rolodex/static/js/main.19ff29cb.chunk.js"
+  },
+  {
+    "revision": "96d8c3c4a6095ec5f160",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.ed572be7.js"
+  }
+]);
